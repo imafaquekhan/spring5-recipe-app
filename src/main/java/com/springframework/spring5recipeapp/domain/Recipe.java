b9456f2498daac08f56a29c3f87d5b6bb1479bd3
mak199 <mak.khan199@googlemail.com>
2021-04-14 12:24:51 +0200

    Implementing Recipe and Category ManyToMany Relationship. closes #6

--- src/main/java/com/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/springframework/spring5recipeapp/domain/Recipe.java
@@ -31,6 +31,12 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    // joinTable because two tables created by jpa of recipe_category and category_recipe due to bidirectional many-to-many relationship
+    @JoinTable(name="recipe_category",
+        joinColumns = @JoinColumn(name="recipe_id"),
+        inverseJoinColumns = @JoinColumn(name="category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -126,5 +132,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
