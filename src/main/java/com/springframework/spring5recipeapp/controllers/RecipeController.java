8d41d95fc5b442127d9dedb8ad0179afa2e1e002
mak199 <mak.khan199@googlemail.com>
2021-04-23 11:51:12 +0200

    add methods in recipeController to view and submit recipeForm. closes #26

--- src/main/java/com/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,11 @@
 package com.springframework.spring5recipeapp.controllers;
 
+import com.springframework.spring5recipeapp.commands.RecipeCommand;
+import com.springframework.spring5recipeapp.domain.Recipe;
 import com.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -18,5 +19,19 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new Recipe());
+        return "recipe/recipeform";
+    }
+
+    // attributes of form are automatically bound to RecipeCommand by the help of naming convention
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/"+savedCommand.getId();
     }
 }
